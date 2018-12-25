c705a58b999cc2aec0ad59fa35bce5d3cf64503e
collier-watkins <collier.watkins.chc@gmail.com>
2018-12-25 14:27:41 -0600

    Tutorial progress through 46:41

--- react-test/src/components/counter.jsx
+++ react-test/src/components/counter.jsx
@@ -1,13 +1,28 @@
 import React, {Component} from 'react';
 
 export default class Counter extends Component {
+	state = {
+		count: 0,
+		imageUrl: 'https://picsum.photos/200'
+	};
+
+	styles = {
+		fontSize: 20,
+		fontWeight: "bold"
+	};
+
 	render() {
 		return ( 
-			<div>
-				<h1>Hello from Counter Class</h1>
-				<button>Increment</button>
-			</div>
+			<React.Fragment>
+				<img src={this.state.imageUrl} alt=""/>
+				<span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+				<button style={{fontSize: 20}} className="btn btn-secondary btn-sm">Increment</button>
+			</React.Fragment>
 		);
+	}
+
+	formatCount(){
+		return this.state.count === 0 ? 'Zero' : this.state.count;
 	}
 }
 
